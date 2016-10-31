1c950205ea741121c4495d8b2a8eb5ebef914b4a
Riotpunchbeats <Ayden.Green@me.com>
2016-10-31 10:07:52 -0400

    got it working, thank god

--- DONE AND WORKING/19.py
+++ DONE AND WORKING/19.py
@@ -1,12 +1,6 @@
-""""99 Bottles of Beer" is a traditional song in the United States and Canada. It is popular to sing on long trips, as it has a very repetitive format which is easy to memorize, and can take a long time to sing. The song's simple lyrics are as follows:
-
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down, pass it around, 98 bottles of beer on the wall.
-
-The same verse is repeated, each time with one fewer bottle. The song is completed when the singer or singers reach zero."""
-
-for n in range(99, 0, -1):
-    print n, 'bottle%s of beer on the the wall.' % ('s', '')[n == 1]
-    print n, 'bottle%s of beer.' % ('s', '')[n == 1]
-    print 'Take one down, pass it around.'
-    print n - 1, 'bottle%s of beer on the wall.\n' % ('s', '')[n - 1 == 1]
+def singBottles():
+    for i in range(99,0,-1): #Sets range and increment to decrese by
+        print(str(i)+"bottles of beer on the wall, "+str(i) +" bottles of beer.") #prints verse with current number of bottles
+        print("Take one down pass it around," +str(i-1) +" bottles of beer on the wall.") #prints verse with one less bottle
+        print(" ")
+singBottles()
