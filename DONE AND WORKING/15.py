36fc7534ad456f561e615fa083f3e08500c2f87d
Riotpunchbeats <Ayden.Green@me.com>
2016-10-31 10:00:53 -0400

    For some reason input was breaking it

--- DONE AND WORKING/15.py
+++ DONE AND WORKING/15.py
@@ -1,5 +1,6 @@
-inp = input("Please enter a string, with spaces: ")
+inp = raw_input("Please enter a string, with spaces: ")
 def find_longest_word(a_string):
     return max(len(word) for word in a_string.split())
 print ("The longest word entered has a length of:")
 print(find_longest_word(inp))
+
