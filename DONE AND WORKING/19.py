2bb9c245084c36ba0a2361d14e68076862242f17
Riotpunchbeats <Ayden.Green@me.com>
2016-10-31 13:51:54 -0400

    Formatting

--- DONE AND WORKING/19.py
+++ DONE AND WORKING/19.py
@@ -1,6 +1,6 @@
 def singBottles():
     for i in range(99,0,-1): #Sets range and increment to decrese by
-        print(str(i)+"bottles of beer on the wall, "+str(i) +" bottles of beer.") #prints verse with current number of bottles
+        print(str(i)+" bottles of beer on the wall, "+str(i) +" bottles of beer.") #prints verse with current number of bottles
         print("Take one down pass it around," +str(i-1) +" bottles of beer on the wall.") #prints verse with one less bottle
         print(" ")
 singBottles()
