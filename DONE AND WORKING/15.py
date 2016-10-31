5cd553ed264732c139b72e8ed850d7b5cc035016
Riotpunchbeats <Ayden.Green@me.com>
2016-10-31 10:02:41 -0400

    .str added

--- DONE AND WORKING/15.py
+++ DONE AND WORKING/15.py
@@ -1,4 +1,4 @@
-inp = raw_input("Please enter a string, with spaces: ")
+inp = str(input("Please enter a string, with spaces: "))
 def find_longest_word(a_string):
     return max(len(word) for word in a_string.split())
 print ("The longest word entered has a length of:")
