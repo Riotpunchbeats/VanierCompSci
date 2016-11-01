596c186d82ed243056f1bb690967729e8465d09e
Riotpunchbeats <Ayden.Green@me.com>
2016-11-01 09:49:50 -0400

    still not working

--- DONE AND WORKING/12.py
+++ DONE AND WORKING/12.py
@@ -1,10 +1,11 @@
+#histogram function
 inp = int(input("Please enter some numbers: ").split())
 def histogram(inputList):
  
-    for i in range(len(inputList)):
-        print (inputList[i]*'*')
+    for i in range(len(inputList)): #for the amount of each number in list, iterate over list
+        print (inputList[i]*'*')#prints histogram based on list
  
  
-List = [inp]
+List = inp
  
 histogram(List)
