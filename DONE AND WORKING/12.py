e14e39302875560d97a930a9c1df15aa06465657
Riotpunchbeats <Ayden.Green@me.com>
2016-11-01 10:10:44 -0400

    *Shrug emote*

--- DONE AND WORKING/12.py
+++ DONE AND WORKING/12.py
@@ -1,11 +1,7 @@
 #histogram function
-inp = int(input("Please enter some numbers: ").split())
-def histogram(inputList):
- 
-    for i in range(len(inputList)): #for the amount of each number in list, iterate over list
-        print (inputList[i]*'*')#prints histogram based on list
- 
- 
-List = inp
- 
-histogram(List)
+lst = input("Please enter some numbers: ").split(",")
+
+def histogram(lst):
+    for item in lst:
+        print (int(item)*
+histogram(lst)
