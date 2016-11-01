57a7458d19c7148cd978416266379b2a6dc4240b
Riotpunchbeats <Ayden.Green@me.com>
2016-11-01 11:54:00 -0400

    Change source

--- DONE AND WORKING/12.py
+++ DONE AND WORKING/12.py
@@ -1,7 +1,9 @@
-#histogram function
-lst = input("Please enter some numbers: ").split(",")
-
-def histogram(lst):
-    for item in lst:
-        print (int(item)*
-histogram(lst)
+def histogram(inputList):
+ 
+    for i in range(len(inputList)):
+        print (inputList[i]*'*')
+ 
+ 
+List = input("Please enter some numbers with spaces: ").split(",")
+print (List)
+histogram(List)
