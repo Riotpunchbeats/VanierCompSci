9dde0fab7957818aae9a8e33eebf65893dc16ae1
Riotpunchbeats <Ayden.Green@me.com>
2016-11-02 10:09:22 -0400

    Somthing teacher wanted to try

--- DONE AND WORKING/28.py
+++ DONE AND WORKING/28.py
@@ -3,7 +3,7 @@
 ips = [int(x) for x in input("Please enter a list of numbers you'd like evaluated, with spaces: ").split()] #Actual split function. Iterates over input and returns a list
 
 from functools import reduce #IMPORT HIGHER ORDER FUNCTION REDUCE
-
+import math
 def max_in_list(numbers):
     return reduce(max, numbers) #Higher order function reduce() used for finding max. 
 
