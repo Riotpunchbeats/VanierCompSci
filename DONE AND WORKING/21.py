dd9539e53e3b75518373ca81de210fffdd2841e8
Riotpunchbeats <Ayden.Green@me.com>
2016-11-02 11:56:50 -0400

    Added comment

--- DONE AND WORKING/21.py
+++ DONE AND WORKING/21.py
@@ -2,7 +2,7 @@ inp = input("Enter a word you would like to evaluate: ")
 def char_freq(str):
     freq = {}
     for c in str: #iterates over letters, because words are just lists 
-        freq[c] = freq.get(c, 0) + 1 #Gets frequency of letters
+        freq[c] = freq.get(c, 0) + 1 #Gets frequency of letters with .get funtion
     return freq
 
 print(char_freq(inp))
