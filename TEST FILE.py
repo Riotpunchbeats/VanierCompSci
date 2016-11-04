565c35a59da2b4eb4d055cb365e5ecd8b21ab0ff
Riotpunchbeats <Ayden.Green@me.com>
2016-11-04 09:30:33 -0400

    Having visual studio python issues

--- TEST FILE.py
+++ TEST FILE.py
@@ -1,3 +1,10 @@
-mylist = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print (mylist[5:])
+class Dog_info: 
+    def __init__(self):
+        def dog(self, breed, age, gender, eye_colour):
+            self.breed = breed
+            self.age = age
+            self.gender = gender
+            self.eye_colour = eye_colour
+    dogType = Dog_info()
+    dogType = dog("golden", 8, "male", "brown")
